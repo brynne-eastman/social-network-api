e84ba6bda9e832b9049727557899527d434e0f80
Brynne Eastman <brynne.eastman@gmail.com>
2021-12-25 08:12:06 -0500

    fixed error in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const express = require('express');
 
-const app = express;
+const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
